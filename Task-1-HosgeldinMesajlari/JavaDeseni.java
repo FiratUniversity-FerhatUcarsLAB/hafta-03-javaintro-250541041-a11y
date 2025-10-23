b8e547e738258984d867feb2f0da709c36ab8e41
250541041-a11y <250541041@firat.edu.tr>
2025-10-23 09:32:59 +0300

    JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,12 @@
+public class JavaDeseni  {
+    public static void main(String[] args) {
+        System.out.println("    j     ");
+        System.out.println("   jav    ");
+        System.out.println("  javaja  ");
+        System.out.println(" javajava ");
+        System.out.println("javvajavva");
+        System.out.println("    ja    ");
+        System.out.println("    va    ");
+
+    }
+}
