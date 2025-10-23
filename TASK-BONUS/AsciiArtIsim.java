3f9c46ef778b0d61c0730e723671f0b08fadfe61
250541041-a11y <250541041@firat.edu.tr>
2025-10-23 11:12:23 +0300

    AsciiArtIsim.java

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+        System.out.println("EEEE  M   M  III  RRRR   EEEEE");
+        System.out.println("E     MM MM   I   R   R  E");
+        System.out.println("EEE   M M M   I   RRRR   EEEE");
+        System.out.println("E     M   M   I   R R    E");
+        System.out.println("EEEE  M   M  III  R  R   EEEEE");
+    }
+}
