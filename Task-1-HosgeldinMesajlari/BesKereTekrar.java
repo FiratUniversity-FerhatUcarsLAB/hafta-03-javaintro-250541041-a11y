ed5976404c872b0be7791c53943d66bccd096a71
250541041-a11y <250541041@firat.edu.tr>
2025-10-23 09:32:13 +0300

    BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class BeskereTekrar  {
+    public static void main(String[] args) {
+        System.out.println("java'ya hosgeldiniz");
+        System.out.println("java'ya hosgeldiniz");
+        System.out.println("java'ya hosgeldiniz");
+        System.out.println("java'ya hosgeldiniz");
+        System.out.println("java'ya hosgeldiniz");
+    }
+}
