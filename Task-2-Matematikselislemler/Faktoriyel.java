fb85a2bb8c8a8b6ca796537db547be92c917c0ae
250541041-a11y <250541041@firat.edu.tr>
2025-10-23 09:54:28 +0300

    Faktoriyel.java

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,7 @@
+public class Faktoriyel {
+    public static void main (String [] args ){
+        int sonuc = 6*5*4*3*2*1;
+       System.out.println("6!  = " + sonuc);    
+        }
+        
+    }
