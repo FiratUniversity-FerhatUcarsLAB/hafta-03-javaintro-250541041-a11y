d33d893794871ed77e2cd0d1769de1b668220cc6
250541041-a11y <250541041@firat.edu.tr>
2025-10-23 09:44:06 +0300

    UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,8 @@
+public class UckereTekrar  {
+    public static void main(String[] args) {
+        System.out.println("programlamaya hoşgeldiniz");
+        System.out.println("programlama eglencelidir");
+        System.out.println("ALGO-LAB dersine hoşgeldiniz");
+
+    }
+}
