9fe7bd441030c5de789ff4eb7a6c0ae25d5ee1e1
250541041-a11y <250541041@firat.edu.tr>
2025-10-23 10:32:29 +0300

    IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main (String []args){
+        String ad = " Emire " ;
+        String soyad = " Şeker " ;
+        String ogrno = " 250541041 " ;
+        String bolum = " Yazilim Muhendisi " ;
+        
+        System.out.println("=====================") ;
+        System.out.println("  OGRENCI BILGILERI  ") ;
+        System.out.println("=====================") ;
+        System.out.println("Ad      : "  + ad     ) ;
+        System.out.println("Soyad   : "  + soyad  ) ;
+        System.out.println("No      : "  + ogrno  ) ;
+        System.out.println("Bolum   : "  + bolum  ) ;
+        System.out.println("=====================") ;
+        }
+    }    
